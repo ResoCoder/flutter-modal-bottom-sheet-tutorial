fa3d9cbc498b4fc53539bbd54ece8fdbaf4609cb
Matej Resetar <mresoo99@gmail.com>
2019-01-30 18:56:29 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _selectedItem = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Show'),
+              onPressed: () => _onButtonPressed(),
+            ),
+            Text(
+              _selectedItem,
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onButtonPressed() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            color: Color(0xFF737373),
+            height: 180,
+            child: Container(
+              child: _buildBottomNavigationMenu(),
+              decoration: BoxDecoration(
+                color: Theme.of(context).canvasColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(10),
+                  topRight: const Radius.circular(10),
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  Column _buildBottomNavigationMenu() {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.ac_unit),
+          title: Text('Cooling'),
+          onTap: () => _selectItem('Cooling'),
+        ),
+        ListTile(
+          leading: Icon(Icons.accessibility_new),
+          title: Text('People'),
+          onTap: () => _selectItem('People'),
+        ),
+        ListTile(
+          leading: Icon(Icons.assessment),
+          title: Text('Stats'),
+          onTap: () => _selectItem('Stats'),
+        ),
+      ],
+    );
+  }
+
+  void _selectItem(String name) {
+    Navigator.pop(context);
+    setState(() {
+      _selectedItem = name;
+    });
+  }
+}
